2e20303e0809824a563365e0c7e1ed8bae379082
zanetagebka <gebka@jchsoft.cz>
2021-12-20 19:37:03 +0100

    - visual changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import './widgets/new_transaction.dart';
@@ -101,7 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
            if (isLandscape) Row(
               children: <Widget>[
                 Text('Show Chart'),
-                Switch(
+                Switch.adaptive(
                     value: _showChart,
                     onChanged: (val) {
                       setState(() {
@@ -123,7 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: Platform.isIOS ? Container() : FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () => _startAddNewTransaction(context),
       ),
